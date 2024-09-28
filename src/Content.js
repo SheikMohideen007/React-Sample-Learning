5ec48b9d92ee1a765eeed48d253d2a56e5e8e283
SheikMohideen007 <sheikmohideen20010@gmail.com>
2024-09-28 12:32:46 +0530

    done for now

--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Content() {
-  return <div>Content</div>;
+  function greetings() {
+    let arr = ["Hi", "Hello", "Welcome"];
+    const random = Math.floor(Math.random() * 3);
+
+    return arr[random];
+  }
+  function handleClick() {
+    console.log("Just Clicked Event Happened...");
+  }
+  function handleClick2(name) {
+    console.log(`Thanks For Clicking ${name}`);
+  }
+  return (
+    <div>
+      <p>Content is {greetings()}</p>
+      <button onClick={() => handleClick()}>Click Me Bro</button>
+    </div>
+  );
 }
 
 export default Content;
