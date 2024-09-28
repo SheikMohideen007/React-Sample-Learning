0822ace5a04c85cf9572c73498da851aab5425eb
SheikMohideen007 <sheikmohideen20010@gmail.com>
2024-09-28 12:22:36 +0530

    Done First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
+import Content from "./Content";
 
 function App() {
+  function greetings() {
+    let arr = ["Hi", "Hello", "Welcome"];
+    const random = Math.floor(Math.random() * 3);
+
+    return arr[random];
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Content />
     </div>
   );
 }
